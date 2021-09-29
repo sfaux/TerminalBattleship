1f07a5f4910a685af4cf11768eb59e4bc725a7f1
jackwarren430 <90930942+jackwarren430@users.noreply.github.com>
2021-09-29 12:03:15 -0400

    ship/square_class

--- Ship.java
+++ Ship.java
@@ -1,3 +1,29 @@
 public class Ship {
-	
+	int size;
+	ArrayList<Square> squares;
+	int x1, y1, x2, y2
+
+	public Ship(int x1, int y1, int x2, int y2, int size){
+		this.size = size;
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+		findCoord();
+	}
+
+	public void findCoord(){
+		squares = new ArrayList<Square>(size);
+		
+	}
+
+	public boolean hasBeenSunk(){
+		for (s : squares){
+			if (s.getHasBeenHit() == false){
+				return false;
+			}
+		}
+		return true;
+	}
+
 }
--- Square.java
+++ Square.java
@@ -1,3 +1,45 @@
 public class Square {
+	private int x, y;
+	private boolean hasShip, hasBeenHit;
 
+	public Square(x, y){
+		this.x = x;
+		this.y = y;
+		hasShip = false;
+		hasBeenHit = false;
+	}
+
+	public int getx(){
+		return x;
+	}
+
+	public void setx(int x){
+		this.x = x;
+	}
+
+	public int gety(){
+		return y;
+	}
+
+	public void sety(){
+		this.y = y;
+	}
+
+	public boolean getHasShip(){
+		return hasShip();
+	}
+
+	public void sethasShip(boolean bool){
+		hasShip = bool;
+	}
+
+	public boolean getHasBeenHit(){
+		return hasBeenHit;
+	}
+
+	public void setHasBeenHit(boolean bool){
+		hasBeenHit = bool;
+	}
+
+	
 }
